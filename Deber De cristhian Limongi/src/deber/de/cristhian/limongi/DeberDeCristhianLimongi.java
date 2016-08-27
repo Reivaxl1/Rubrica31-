ea773a53ae32e8d4e6957d8da315665752ea9bd2
Cristhian <Cristhian@DESKTOP-UVM7PUO>
2016-08-26 20:23:14 -0500

    Profesor este soy yo Cristhian Limongi este usuario fue con el que cree la cuenta el dia de clase =D

---
+++ Deber De cristhian Limongi/src/deber/de/cristhian/limongi/DeberDeCristhianLimongi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deber.de.cristhian.limongi;
+
+/**
+ *
+ * @author Cristhian
+ */
+public class DeberDeCristhianLimongi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         // TODO code application logic here
+        int numero=0,numero1=0,numero2=0,mayor=0;
+        //ingresar los 3 numeros 
+        numero=1;
+        numero1=2;
+        numero2=3;
+        //hallar el mayor
+    }
+    
+}
